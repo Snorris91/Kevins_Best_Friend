e3e44cbbfa99dcda19a77300fc5403d3853b06ce
Spencer Norris <Spencer.serv@gmail.com>
2023-10-27 15:17:55 -0500

    updated declarative

--- src/services/haulerService.jsx
+++ src/services/haulerService.jsx
@@ -6,16 +6,16 @@ export const getHaulerById = (haulerId) => {
     return fetch(`http://localhost:8000/haulers/${haulerId}`).then(res => res.json())
 }
 
-// export const editHauler = (hauler) => {
-//     return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(hauler),
-//       }).then((res) => res.json());
-// }
-
+export const editHauler = (hauler) => {
+    return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(hauler),
+      })
+}
+// .then((res) => res.json());
 // export const editHauler = (hauler) => {
 //     return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
 //       method: "PUT",
@@ -36,26 +36,26 @@ export const getHaulerById = (haulerId) => {
 //       });
 //   }
 
-export const editHauler = (hauler) => {
-    return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(hauler),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-        try {
-          return res.json();
-        } catch (error) {
-          throw new Error("Failed to parse JSON response");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // Handle the error appropriately
-      });
-  }
+// export const editHauler = (hauler) => {
+//     return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(hauler),
+//     })
+//       .then((res) => {
+//         if (!res.ok) {
+//           throw new Error(`HTTP error! Status: ${res.status}`);
+//         }
+//         try {
+//           return res.json();
+//         } catch (error) {
+//           throw new Error("Failed to parse JSON response");
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//         // Handle the error appropriately
+//       });
+//   }
