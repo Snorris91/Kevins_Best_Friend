3032f0f3e36327609377255b2b64fc542a8bcd4c
Spencer Norris <Spencer.serv@gmail.com>
2023-10-29 10:29:47 -0500

    Ships finished

--- src/dockView/Docks.jsx
+++ src/dockView/Docks.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
-import { getAllDocks } from "../services/dockService";
+import { getEmbedDocks } from "../services/dockService";
 import { useEffect } from "react";
 
 export const AllDocks = () => {
   const [allDocks, setAllDocks] = useState([]);
 
   useEffect(() => {
-    getAllDocks().then((dockArray) => {
+    getEmbedDocks().then((dockArray) => {
       setAllDocks(dockArray);
+      console.log(dockArray);
     });
-
-    
   }, []);
 
   return (
-    <div>
+    <div className="docks text-center">
       <ul>
         {allDocks.map((dock) => {
           return (
             <li key={dock.id} value={dock.id}>
-              {dock.location}: {dock.capacity}
+              {dock.location}- Capacity: {dock.capacity} Tons <br></br> Hauler
+              Name:<br></br>
+              {dock.haulers?.map((hauler) => (
+                <li key={hauler.id} className="hauler_name ">
+                  {hauler.name}
+                </li>
+              ))}
             </li>
           );
         })}
--- src/services/dockService.jsx
+++ src/services/dockService.jsx
@@ -1,3 +1,7 @@
 export const getAllDocks = () => {
     return fetch("http://localhost:8000/docks").then(res => res.json())
 }
+
+export const getEmbedDocks = () => {
+    return fetch("http://localhost:8000/docks?_embed=haulers").then(res => res.json())
+}
